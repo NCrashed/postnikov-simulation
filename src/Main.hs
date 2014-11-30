a93b1a80dd724ea1ec66ae811fa49d93374a4e04
NCrashed <ncrashed@gmail.com>
2014-11-30 05:12:07 +0300

    Russian labels

--- src/Main.hs
+++ src/Main.hs
@@ -16,17 +16,17 @@ main = gui
 editingScreen :: Collection -> (Input -> IO ()) -> IO () -> IO (IO ())
 editingScreen collection okAction cancelAction = do
   fg <- newFocusGroup
-  tbl <- newTable [column (ColFixed 20), column ColAuto] BorderFull
-  afterQueryTimeEdit <- labeledText fg tbl "afterQueryTime" "80"
-  formQueryTimeEdit <- labeledText fg tbl "formQueryTime" "80"
-  sendingTimeEdit <- labeledText fg tbl "sendingTime" "10"
-  serversCountEdit <- labeledText fg tbl "serversCount" "2"
-  workstationsCountEdit <- labeledText fg tbl "workstationsCount" "8"
-  processorsCountEdit <- labeledText fg tbl "processorsCount"  "1"
-  processoringTimeEdit <- labeledText fg tbl "processoringTime" "10"
-  diskingTimeEdit <- labeledText fg tbl "diskingTime" "10"
-  disksCountEdit <- labeledText fg tbl "disksCount" "1"
-  requeryChanceEdit <- labeledText fg tbl "requeryChance" "0.1"
+  tbl <- newTable [column ColAuto, column ColAuto] BorderFull
+  afterQueryTimeEdit <- labeledText fg tbl "Время дообработки запроса" "80"
+  formQueryTimeEdit <- labeledText fg tbl "Время формирования запроса" "80"
+  sendingTimeEdit <- labeledText fg tbl "Время отправки по каналу" "10"
+  serversCountEdit <- labeledText fg tbl "Кол-во серверов" "2"
+  workstationsCountEdit <- labeledText fg tbl "Кол-во станций" "8"
+  processorsCountEdit <- labeledText fg tbl "Кол-во процессоров"  "1"
+  processoringTimeEdit <- labeledText fg tbl "Время обработки ЦПУ" "10"
+  diskingTimeEdit <- labeledText fg tbl "Время обработки диском" "10"
+  disksCountEdit <- labeledText fg tbl "Кол-во дисков" "1"
+  requeryChanceEdit <- labeledText fg tbl "Вероятность повтора" "0.1"
   
   title <- hCentered =<< plainText "Enter model parameters"
   
@@ -78,19 +78,19 @@ editingScreen collection okAction cancelAction = do
 resultsScreen :: Collection -> IO () -> IO () -> IO (IO (), Input -> IO ())
 resultsScreen collection backAction exitAction = do
   fg <- newFocusGroup
-  tbl <- newTable [column (ColFixed 20), column ColAuto, column ColAuto, column ColAuto] BorderFull
-  title0 <- plainText "Name"
-  title1 <- plainText "Analytic"
-  title2 <- plainText "Simulation"
-  title3 <- plainText "Difference"
+  tbl <- newTable [column ColAuto, column ColAuto, column ColAuto, column ColAuto] BorderFull
+  title0 <- plainText "Параметр модели"
+  title1 <- plainText "Аналитическая модель"
+  title2 <- plainText "Имитационная модель"
+  title3 <- plainText "Разница моделей"
   addRow tbl $ title0 .|. title1 .|. title2 .|. title3
-  (al1, sl1, dl1) <- row tbl "averageResponse"
-  (al2, sl2, dl2) <- row tbl "loadWorkstation"
-  (al3, sl3, dl3) <- row tbl "loadUser"
-  (al4, sl4, dl4) <- row tbl "loadCable"
-  (al5, sl5, dl5) <- row tbl "loadServer"
-  (al6, sl6, dl6) <- row tbl "loadDisk"
-  (al7, sl7, dl7) <- row tbl "loadCP"
+  (al1, sl1, dl1) <- row tbl "Время отклика системы"
+  (al2, sl2, dl2) <- row tbl "Загрузка рабочей станции"
+  (al3, sl3, dl3) <- row tbl "Загрузка пользователя"
+  (al4, sl4, dl4) <- row tbl "Загрузка канала"
+  (al5, sl5, dl5) <- row tbl "Загрузка сервера"
+  (al6, sl6, dl6) <- row tbl "Загрузка дисков"
+  (al7, sl7, dl7) <- row tbl "Загрузка процессора"
   
   bBack <- newButton "Back"
   void $ addToFocusGroup fg (buttonWidget bBack)
@@ -141,14 +141,14 @@ resultsScreen collection backAction exitAction = do
       setText l7 $ T.pack $ show $ loadCP
     
     fillDifference o1 o2 l1 l2 l3 l4 l5 l6 l7 = do
-      let percent v1 v2 = abs (v1 - v2) / v1
-      setText l1 $ T.pack $ show $ percent (averageResponse o1) (averageResponse o2)
-      setText l2 $ T.pack $ show $ percent (loadWorkstation o1) (loadWorkstation o2)
-      setText l3 $ T.pack $ show $ percent (loadUser o1) (loadUser o2)
-      setText l4 $ T.pack $ show $ percent (loadCable o1) (loadCable o2)
-      setText l5 $ T.pack $ show $ percent (loadServer o1) (loadServer o2)
-      setText l6 $ T.pack $ show $ percent (loadDisk o1) (loadDisk o2)
-      setText l7 $ T.pack $ show $ percent (loadCP o1) (loadCP o2)
+      let percent v1 v2 = T.pack $ show (100 * abs (v1 - v2) / v1) ++ "%"
+      setText l1 $ percent (averageResponse o1) (averageResponse o2)
+      setText l2 $ percent (loadWorkstation o1) (loadWorkstation o2)
+      setText l3 $ percent (loadUser o1) (loadUser o2)
+      setText l4 $ percent (loadCable o1) (loadCable o2)
+      setText l5 $ percent (loadServer o1) (loadServer o2)
+      setText l6 $ percent (loadDisk o1) (loadDisk o2)
+      setText l7 $ percent (loadCP o1) (loadCP o2)
       
 gui :: IO()
 gui =  do
